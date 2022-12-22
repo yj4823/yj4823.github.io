e448e036d6e2b684a25ab20b772d94838b1be8a4
yj4823 <112037390+yj4823@users.noreply.github.com>
2022-12-22 17:50:47 +0900

    Delete 카지노펍 directory

--- 카지노펍/js/menu.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-
-    $('.navi_bar a').click(function(){
-        $('.navi').css('right','0');
-    });
-    $('.menu_close').click(function(){
-        $('.navi').css('right','-100%')
-    });
-});
